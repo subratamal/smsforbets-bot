7e27d4520eed8d19c0d0d2e0f3b6e6e07737ab06
Subrat <06.subrata@gmail.com>
2018-07-05 03:50:36 +0530

    initial tooling setup done

---
+++ config/webpack.config.js
@@ -0,0 +1,49 @@
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+// const glob = require('glob')
+
+let entry = path.resolve('./src/index.js')
+// if (process.env.NODE_ENV === 'test') {
+//   entry = glob.sync('./test/**/*.js')
+// }
+
+module.exports = {
+  entry,
+  target: 'node',
+  // Generate sourcemaps for proper error messages
+  devtool: 'source-map',
+  // Since 'aws-sdk' is not compatible with webpack,
+  // we exclude all node dependencies
+  externals: [nodeExternals()],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  optimization: {
+    // We no not want to minimize our code.
+    minimize: false,
+  },
+  performance: {
+    // Turn off size warnings for entry points
+    hints: false,
+  },
+  // Run babel on all .js files and skip those in node_modules
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      models: path.join(__dirname, '/src/models'),
+      libs: path.join(__dirname, '/src/libs'),
+      controllers: path.resolve(__dirname, 'src/controllers'),
+      managers: path.resolve(__dirname, 'src/managers'),
+      middlewares: path.resolve(__dirname, 'src/middlewares'),
+      utils: path.resolve(__dirname, 'src/utils'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+    },
+  },
+}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import path from 'path'
+import fs from 'fs-extra'
+import { puppeteerMeta, pageMeta } from './../config/dev.config.json'
