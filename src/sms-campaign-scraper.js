14c73d87079ca0722705050310c3900180ba7b8a
Subrat <06.subrata@gmail.com>
2018-07-05 22:07:20 +0530

    campaign scraper methods added

--- src/sms-campaign-scraper.js
+++ src/sms-campaign-scraper.js
@@ -1,9 +1,13 @@
 import path from 'path'
 import fs from 'fs-extra'
 import yaml from 'js-yaml'
+import to from 'await-to-js'
 import puppeteer from 'puppeteer'
-import { EventEmitter } from 'events'
+import {
+  EventEmitter 
+} from 'events'
 import Logger from './utils/logger'
+import SMSCampaignManager from './managers/sms-campaign-data'
 
 const logger = Logger('sms_campaign_logger')
 
@@ -17,11 +21,12 @@ const resolution = {
 }
 
 export default class SMSCampaignScraper extends EventEmitter {
-  constructor(browser = {}, page = {}) {
+  constructor(campaignTransactions = [], browser = {}, page = {}, ) {
     super();
     this.browser = browser;
     this.page = page;
     this.site = jsonConfig['site_meta']
+    this.campaignTransactions = campaignTransactions
   }
 
   async createBrowser(options = {}) {
@@ -43,6 +48,10 @@ export default class SMSCampaignScraper extends EventEmitter {
     logger.info(`Browser instance created. Version: ${await this.browser.version()}`)
   }
 
+  async closeBrowser() {
+    await this.browser.close();
+  }
+
   async createPage(options) {
     const {
       width,
@@ -50,15 +59,18 @@ export default class SMSCampaignScraper extends EventEmitter {
       setBypassCSP,
     } = options;
 
-    this.page = await this.browser.newPage();
-    await this.page.setViewport({
+    const page = await this.browser.newPage();
+    await page.setViewport({
       width,
       height,
     });
-    await this.page.setBypassCSP(setBypassCSP);
+    await page.setBypassCSP(setBypassCSP);
+    return page
   }
 
   async init(browserOptions = {}, pageOptions = {}) {
+    this.pageOptions = pageOptions
+
     const {
       headless,
       userDataDir,
@@ -77,26 +89,100 @@ export default class SMSCampaignScraper extends EventEmitter {
       executablePath,
     });
 
-    await this.createPage({
+    this.page = await this.createPage({
       width,
       height,
       setBypassCSP,
     });
 
-    await this.page.goto(this.site.login_url, {
-      waitUntil: 'networkidle2',
-    });
-
     // Login
-    await this.page.type('#loginAreax > div:nth-child(3) > div > input', this.site.username);
-    await this.page.type('#loginAreax > div:nth-child(4) > div > input', this.site.password);
-    await this.page.click('#girisyap');
-    await this.page.waitForNavigation();
+    try {
+      await this.login(this.page)
+    } catch (err) {
+      logger.info('Login failed. Exiting')
+      await this.closeBrowser()
+    }
 
-    await this.page.goto(this.site.post_url, {
-      waitUntil: 'networkidle2',
-    })
+    try {
+      // Get cookies
+      this.cookies = await this.page.cookies()
+
+      await this.page.goto(this.site.post_url, {
+        waitUntil: 'networkidle2',
+      })
+
+      await this.scrape()
+    } catch (err) {
+      logger.info('exiting due to scraping error. remaining campaigns will be processed in the next run')
+    } finally {
+      await this.closeBrowser()
+    }
 
     return this;
+  }
+
+  async login(page) {
+    await page.goto(this.site.login_url, {
+      waitUntil: 'networkidle2',
+    });
+    await page.type('#loginAreax > div:nth-child(3) > div > input', String(this.site.username));
+    await page.type('#loginAreax > div:nth-child(4) > div > input', this.site.password);
+    await page.click('#girisyap');
+    await page.waitForNavigation();
+  }
+
+  async scrape() {
+    const promises = this.campaignTransactions.map(async (campaignTransaction, idx) => {
+      let page = this.page
+      if (idx > 0) {
+        page = await this.createPage(this.pageOptions)
+        await page.setCookie(...this.cookies)
+        await page.goto(this.site.post_url)
+      }
+      await this.fillDetailsPages(page, campaignTransaction)
+      const dataSubmitted = await this.fillApprovalPage(page)
+      if (!dataSubmitted) {
+        return
+      }
+
+      let err, dbUpdated;
+      [err, dbUpdate] = to(await SMSCampaignManager.updateCampaignProcessed(campaignTransaction))
+      if (err) {
+        logger.info(`campaign status update failed for campaign Id: ${campaignTransaction.id}`)
+      }
+      logger.info(`campaign status updated successfully for campaign Id: ${campaignTransaction.id} with mobile numbers
+        ${campaignTransaction.mobileNumbers}`)
+
+      await page.deleteCookie(...this.cookies)
+    })
+
+    await Promise.all(promises)
+  }
+
+  async fillDetailsPages(page, campaignTransaction) {
+    await page.type('#frmMesajGonder > div:nth-child(3) > div.panel-body > div > textarea', String(campaignTransaction.mobileNumbers))
+    await page.type('#mesaj', campaignTransaction.messageText)
+    await page.click('#btnSubmit')
+    await page.waitForNavigation()
+  }
+
+  async fillApprovalPage(page) {
+    await page.click('#ortaDetay > form > div:nth-child(4) > div:nth-child(3) > input')
+    await page.waitForNavigation()
+
+    let dataSubmitted = true
+    let [err] = await to(page.waitForFunction(`document.body.innerText.includes('Mesajınızın gönderimi başarıyla gerçekleşmiştir')`, {
+      timeout: 5000
+    }))
+
+    if (err) {
+      dataSubmitted = false
+      logger.info(err)
+    } else {
+      logger.info(`campaign details submitted successfully for campaign Id ${campaignTransaction.id} with message text::
+    '${campaignTransaction.messageText}' and mobile numbers:: ${campaignTransaction.mobileNumbers}`)
+    }
+
+    return dataSubmitted
   }
 }
