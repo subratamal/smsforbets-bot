d55c3cbb1a83ff38d83289e58c545c85f4ca591d
Subrat <06.subrata@gmail.com>
2018-07-06 13:51:23 +0530

    run method seperated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import cron from 'node-cron'
 import SMSCampaignManager from './managers/sms-campaign'
+import Logger from './utils/logger'
+import uuidv4 from 'uuid/v4'
+
+const _runId = uuidv4()
+const logger = Logger('SMSForBetsCampaign_Bot', {
+  data: {
+    runId: _runId
+  }
+})
+
 
 // cron.schedule('* * * * *', async function () {
 //   const smsCampaignManager = new SMSCampaignManager()
 //   await smsCampaignManager.init()
 // })
 
-(async function () {
-  const smsCampaignManager = new SMSCampaignManager()
-  await smsCampaignManager.init()
-})()
+async function run() {
+  const smsCampaignManager = new SMSCampaignManager({
+    logger
+  })
+  logger.info(`Initalizing SMSForBetsCampaign Bot.`)
+  await smsCampaignManager.init();
+}
+
+run()
